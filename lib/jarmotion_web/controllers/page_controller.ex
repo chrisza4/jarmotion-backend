301e28fae642ca82aba0d7a261943a34602b126d
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2019-10-12 00:38:07 +0700

    Another welcome

--- lib/jarmotion_web/controllers/page_controller.ex
+++ lib/jarmotion_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule JarmotionWeb.PageController do
   use JarmotionWeb, :controller
 
   def index(conn, _params) do
-    send_resp(conn, 200, "Jarmotion up and running. Welcome to Jarmotion :)")
+    send_resp(conn, 200, "Jarmotion up and running. Welcome to Jarmotion :==)")
   end
 end
