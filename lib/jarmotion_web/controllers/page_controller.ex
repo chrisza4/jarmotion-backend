3a4565bd22c6f35041b4215aef6c4a7031880011
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2019-10-11 19:25:56 +0700

    Try change welcome message to test deployment

--- lib/jarmotion_web/controllers/page_controller.ex
+++ lib/jarmotion_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule JarmotionWeb.PageController do
   use JarmotionWeb, :controller
 
   def index(conn, _params) do
-    send_resp(conn, 200, "Jarmotion running")
+    send_resp(conn, 200, "Jarmotion up and running")
   end
 end
