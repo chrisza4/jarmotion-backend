9c27fd75b47162e64fd4187c14b1e8ef38d8f542
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2019-10-11 19:01:07 +0700

    try again

--- deployments/restart_and_deploy.sh
+++ deployments/restart_and_deploy.sh
@@ -3,7 +3,6 @@ mix compile
 mix ecto.migrate
 mix release
 _build/prod/rel/jarmotion/bin/jarmotion stop
-sleep 3
 _build/prod/rel/jarmotion/bin/jarmotion daemon
 
 # MIX_ENV=prod elixir --erl "-detached" -S mix phx.server
