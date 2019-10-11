44faa12403022bbfaf0f9d305d837a6d7af2a17a
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2019-10-12 05:27:33 +0700

    Use HTTPS and better deployment config

--- rel/runtime_config.exs
+++ rel/runtime_config.exs
@@ -10,28 +10,28 @@ config :jarmotion, Jarmotion.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: 10
 
-# config :jarmotion, JarmotionWeb.Endpoint,
-#   https: [
-#     :inet6,
-#     port: 443,
-#     cipher_suite: :strong,
-#     keyfile: {:system, "SOME_APP_SSL_KEY_PATH"},
-#     certfile: {:systen, "SOME_APP_SSL_CERT_PATH"}
-#   ],
-#   # This is critical for ensuring web-sockets properly authorize.
-#   url: [host: "localhost", port: 443],
-#   cache_static_manifest: "priv/static/cache_manifest.json",
-#   server: true,
-#   root: "."
-
-# ==================== For testing release locally =======================
 config :jarmotion, JarmotionWeb.Endpoint,
-  http: [
+  https: [
     :inet6,
-    port: 80
+    port: 443,
+    cipher_suite: :strong,
+    keyfile: {:system, "SOME_APP_SSL_KEY_PATH"},
+    certfile: {:systen, "SOME_APP_SSL_CERT_PATH"}
   ],
   # This is critical for ensuring web-sockets properly authorize.
-  url: [host: "localhost", port: 80],
+  url: [host: "localhost", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   root: "."
+
+# ==================== For testing release locally =======================
+# config :jarmotion, JarmotionWeb.Endpoint,
+#   http: [
+#     :inet6,
+#     port: 80
+#   ],
+#   # This is critical for ensuring web-sockets properly authorize.
+#   url: [host: "localhost", port: 80],
+#   cache_static_manifest: "priv/static/cache_manifest.json",
+#   server: true,
+#   root: "."
