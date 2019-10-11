548a311b915d09b030184cf0e5d8900282fecee0
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2019-10-11 14:57:37 +0700

    Simpler sample page

--- lib/jarmotion_web/controllers/page_controller.ex
+++ lib/jarmotion_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule JarmotionWeb.PageController do
   use JarmotionWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    send_resp(conn, 200, "Jarmotion running")
   end
 end
--- test/jarmotion_web/controllers/page_controller_test.exs
+++ test/jarmotion_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule JarmotionWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert response(conn, 200) =~ "Jarmotion running"
   end
 end
