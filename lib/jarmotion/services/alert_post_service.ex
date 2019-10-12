698a9e8952c5d9d48d63a864ec85b26830912e6e
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2019-10-12 17:08:02 +0700

    Broadcast changes

--- lib/jarmotion/services/alert_post_service.ex
+++ lib/jarmotion/services/alert_post_service.ex
@@ -1,7 +1,7 @@
 defmodule Jarmotion.Service.AlertPostService do
   alias Jarmotion.Schemas.Alert
 
-  def post_add_alert(%Alert{} = _) do
-    {:error, :not_implemented}
+  def post_add_alert(%Alert{} = alert) do
+    JarmotionWeb.Endpoint.broadcast("user:#{alert.to_user_id}", "alert:add", %{id: alert.id})
   end
 end
