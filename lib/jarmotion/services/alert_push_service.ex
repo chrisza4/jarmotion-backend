0112d88757690a2db92ecaae532efc7e73c690eb
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2019-10-18 15:20:41 +0700

    Handle status of alert properly

--- lib/jarmotion/services/alert_push_service.ex
+++ lib/jarmotion/services/alert_push_service.ex
@@ -1,20 +1,31 @@
 defmodule Jarmotion.Service.AlertPushService do
   alias Jarmotion.Schemas.Alert
-  alias Jarmotion.Repo.{DeviceRepo, UserRepo}
+  alias Jarmotion.Repo.{DeviceRepo, UserRepo, AlertRepo}
 
   def send(%Alert{} = alert) do
     owner = UserRepo.get(alert.owner_id)
     devices = DeviceRepo.list_by_user_id(alert.to_user_id)
 
-    Enum.map(devices, fn device ->
-      %{
-        to: device.token,
-        title: "Alert",
-        body: "#{owner.name} just send you an alert!!!"
-      }
-    end)
-    |> ExponentServerSdk.PushNotification.push_list()
+    messages =
+      Enum.map(devices, fn device ->
+        %{
+          to: device.token,
+          title: "Alert",
+          body: "#{owner.name} just send you an alert!!!"
+        }
+      end)
 
-    :ok
+    with {:ok, _} <- ExponentServerSdk.PushNotification.push_list(messages),
+         {:ok, alert} <- AlertRepo.update_status(alert, "pending") do
+      {:ok, alert}
+    else
+      {:error, _message, _status_code} -> handle_failed_send(alert)
+    end
+  end
+
+  defp handle_failed_send(%Alert{} = alert) do
+    with {:ok, _} <- AlertRepo.update_status(alert, "push_failed") do
+      {:error, :push_failed}
+    end
   end
 end
--- test/jarmotion/services/alert_push_service_test.exs
+++ test/jarmotion/services/alert_push_service_test.exs
@@ -2,8 +2,7 @@ defmodule Jarmotion.Service.AlertPushServiceTest do
   use JarmotionWeb.ConnCase, async: true
   alias Jarmotion.TestSetup
   alias Jarmotion.Service.{AlertPushService}
-  alias Jarmotion.Schemas.Alert
-  alias Jarmotion.Mocks
+  alias Jarmotion.Repo.AlertRepo
 
   import Mock
 
@@ -12,17 +11,21 @@ defmodule Jarmotion.Service.AlertPushServiceTest do
                     [
                       %{"status" => "ok", "id" => "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"}
                     ]}
+    @push_failed_response {:error, "something", 500}
+
     setup %{} do
       {:ok, chris} = TestSetup.create_user(%{email: "chris@test.com", name: "chris"})
       {:ok, awa} = TestSetup.create_user(%{email: "awa@test.com", name: "Awa"})
+      {:ok, alert} = TestSetup.create_alert(awa.id, chris.id)
       {:ok, _} = TestSetup.create_device(chris.id, "mytoken")
-      {:ok, awa: awa, chris: chris}
+      {:ok, awa: awa, chris: chris, alert: alert}
     end
 
-    test "should send push notification", %{chris: chris, awa: awa} do
+    test "should send push notification and once success, update alert status to sent", %{
+      alert: alert
+    } do
       with_mock(ExponentServerSdk.PushNotification, push_list: fn _ -> @push_response end) do
-        alert = Mocks.alert_newly_created(awa.id, chris.id)
-        assert :ok == AlertPushService.send(alert)
+        assert {:ok, updated_alert} = AlertPushService.send(alert)
 
         assert_called(
           ExponentServerSdk.PushNotification.push_list([
@@ -33,11 +36,34 @@ defmodule Jarmotion.Service.AlertPushServiceTest do
             }
           ])
         )
+
+        assert updated_alert.status == "pending"
+      end
+    end
+
+    test "should send push notification and once failed, update alert status to failed", %{
+      alert: alert
+    } do
+      with_mock(ExponentServerSdk.PushNotification, push_list: fn _ -> @push_failed_response end) do
+        assert {:error, :push_failed} = AlertPushService.send(alert)
+
+        assert_called(
+          ExponentServerSdk.PushNotification.push_list([
+            %{
+              to: "mytoken",
+              title: "Alert",
+              body: "Awa just send you an alert!!!"
+            }
+          ])
+        )
+
+        updated_alert = AlertRepo.get(alert.id)
+        assert updated_alert.status == "push_failed"
       end
     end
 
     @tag :skip
-    test "Should update status of push notification" do
+    test "Should notify sender that push notification failed" do
     end
   end
 end
