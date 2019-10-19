a58491b7023d32e992cd6bf3aa086bae641652c1
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2019-10-19 17:44:35 +0700

    Add send push by sensor

--- lib/jarmotion/services/sensor_service.ex
+++ lib/jarmotion/services/sensor_service.ex
@@ -1,6 +1,6 @@
 defmodule Jarmotion.Service.SensorService do
   alias Jarmotion.Schemas.Sensor
-  alias Jarmotion.Repo.{SensorRepo, RelationshipRepo, EmojiRepo}
+  alias Jarmotion.Repo.{SensorRepo, RelationshipRepo, EmojiRepo, DeviceRepo}
 
   def upsert_sensor(by_user_id, type, threshold) do
     with {:ok, sensor} <-
@@ -22,20 +22,43 @@ defmodule Jarmotion.Service.SensorService do
     {:ok, SensorRepo.list_by_owner(by_user_id)}
   end
 
-  def list_notifier_ids(owner_id) do
+  def list_trigger_sensors(owner_id) do
     friend_ids = RelationshipRepo.get_friend_ids(owner_id)
     emoji_stats = EmojiRepo.count_by_type_today(owner_id)
     sensors = SensorRepo.list_by_owners(friend_ids)
 
-    owner_ids =
+    trigger_sensors =
       sensors
       |> Enum.filter(fn sensor ->
         Enum.any?(emoji_stats, fn emoji_stat ->
           emoji_stat.type == sensor.emoji_type and emoji_stat.count >= sensor.threshold
         end)
       end)
-      |> Enum.map(& &1.owner_id)
 
-    {:ok, owner_ids}
+    {:ok, trigger_sensors}
+  end
+
+  def send_push_to_sensors(sensors) do
+    push_result =
+      sensors
+      |> Enum.map(& &1.id)
+      |> DeviceRepo.list_by_sensor_ids()
+      |> Enum.map(fn %{sensor: sensor, device: device} ->
+        %{
+          to: device.token,
+          title: "Alert",
+          body: "Your partner is #{sensor.emoji_type}.",
+          data: %{
+            type: "sensor",
+            id: sensor.id
+          }
+        }
+      end)
+      |> ExponentServerSdk.PushNotification.push_list()
+
+    case push_result do
+      {:ok, _} -> :ok
+      {:error, _, _} -> {:error, :push_failed}
+    end
   end
 end
--- test/jarmotion/services/sensor_service_test.exs
+++ test/jarmotion/services/sensor_service_test.exs
@@ -3,6 +3,8 @@ defmodule Jarmotion.Service.SensorServiceTest do
   alias Jarmotion.TestSetup
   alias Jarmotion.Service.SensorService
 
+  import Mock
+
   describe "upsert_sensor" do
     setup %{} do
       {:ok, chris} = TestSetup.create_user(%{email: "chris@test.com"}, "mypassword")
@@ -39,7 +41,7 @@ defmodule Jarmotion.Service.SensorServiceTest do
     end
   end
 
-  describe "list_notifier_ids" do
+  describe "list_trigger_sensors" do
     setup %{} do
       {:ok, chris} = TestSetup.create_user(%{email: "chris@test.com"}, "mypassword")
       {:ok, awa} = TestSetup.create_user(%{email: "awa@test.com"}, "mypassword")
@@ -54,13 +56,59 @@ defmodule Jarmotion.Service.SensorServiceTest do
       chris: chris
     } do
       TestSetup.create_emoji(awa.id, %{type: "enraged"})
-      {:ok, users} = SensorService.list_notifier_ids(awa.id)
+      {:ok, users} = SensorService.list_trigger_sensors(awa.id)
       assert length(users) == 0
 
       TestSetup.create_emoji(awa.id, %{type: "enraged"})
-      {:ok, users} = SensorService.list_notifier_ids(awa.id)
-      assert length(users) == 1
-      assert Enum.at(users, 0) == chris.id
+      {:ok, sensors} = SensorService.list_trigger_sensors(awa.id)
+      assert length(sensors) == 1
+      assert Enum.at(sensors, 0).owner_id == chris.id
+    end
+  end
+
+  describe "send_push_to_sensors" do
+    @push_response {:ok,
+                    [
+                      %{"status" => "ok", "id" => "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"}
+                    ]}
+
+    setup %{} do
+      {:ok, chris} = TestSetup.create_user(%{email: "chris@test.com", name: "chris"})
+      {:ok, _} = TestSetup.create_device(chris.id, "mytoken1")
+      {:ok, _} = TestSetup.create_device(chris.id, "mytoken2")
+      {:ok, sensor} = TestSetup.create_sensor(chris.id, "enraged", 2)
+      {:ok, sensor: sensor}
+    end
+
+    test "Given chris set too enraged to be 2, Should told chris that awa is too enraged", %{
+      sensor: sensor
+    } do
+      with_mock(ExponentServerSdk.PushNotification, push_list: fn _ -> @push_response end) do
+        assert :ok == SensorService.send_push_to_sensors([sensor])
+
+        assert_called(
+          ExponentServerSdk.PushNotification.push_list([
+            %{
+              to: "mytoken1",
+              title: "Alert",
+              body: "Your partner is enraged.",
+              data: %{
+                type: "sensor",
+                id: sensor.id
+              }
+            },
+            %{
+              to: "mytoken2",
+              title: "Alert",
+              body: "Your partner is enraged.",
+              data: %{
+                type: "sensor",
+                id: sensor.id
+              }
+            }
+          ])
+        )
+      end
     end
   end
 end
